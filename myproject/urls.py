f1ca9d36aee5cb0364a9981110e79161786991e0
Narayan Thapa Magar <thmgrnarayan@gmail.com>
2025-01-21 20:20:29 +0545

    signup

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from accounts import views as accounts_views
+
 from boards import views
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^signup/$', accounts_views.signup, name='signup'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     # url(r'^about/$', views.about, name='about'),

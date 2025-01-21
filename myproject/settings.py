752cc9002670f031b21c96c7a27d53e584c8b211
Narayan Thapa Magar <thmgrnarayan@gmail.com>
2025-01-21 20:00:54 +0545

    add application

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'boards',
     'widget_tweaks',
+
+    'accounts',
+    'boards',
 ]
 
 MIDDLEWARE = [
